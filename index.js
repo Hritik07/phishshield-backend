2f52b60d07605330ae75bff555b051f48ae8143f
Hritik Dua <35106576+Hritik07@users.noreply.github.com>
2026-04-12 23:59:08 +0530

    Changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { calculateRisk } = require('./utils/riskEngine');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 const app = express();
 app.use(cors());
